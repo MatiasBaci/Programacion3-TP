b16a253eb00d904ff3641816802c7b031946cadc
smsx42 <92806548+smsx42@users.noreply.github.com>
2023-11-02 16:50:56 -0300

    Delete TpJava/src/main/java/org/fiuba/algoritmos3/Modificacion.java

--- TpJava/src/main/java/org/fiuba/algoritmos3/Modificacion.java
+++
@@ -1,7 +0,0 @@
-package org.fiuba.algoritmos3;
-
-public interface Modificacion {
-    public abstract void modificar(Cualidades cualidades,int etapas);
-
-    public abstract String obtenerNombreModificacion();
-}
