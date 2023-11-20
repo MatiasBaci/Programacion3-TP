1fb81db7a815496189013dc847b7ca4b69f7ebf1
MatiasBaci <matibaciga@gmail.com>
2023-11-20 13:55:28 -0300

    MainJavaFx

---
+++ TpJava/src/main/java/orgFiuba/MainJavaFX.java
@@ -0,0 +1,18 @@
+package orgFiuba;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+public class MainJavaFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
