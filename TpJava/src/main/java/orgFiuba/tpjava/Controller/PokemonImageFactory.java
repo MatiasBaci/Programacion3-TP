0298cb6f821fb32a4c19e515e494d773a540e1bc
MatiasBaci <matibaciga@gmail.com>
2023-11-20 22:09:48 -0300

    algunas PNGs

---
+++ TpJava/src/main/java/orgFiuba/tpjava/Controller/PokemonImageFactory.java
@@ -0,0 +1,7 @@
+package orgFiuba.tpjava.Controller;
+
+import java.util.Map;
+
+public class PokemonImageFactory {
+    //final static Map <Class, String> pokemonesJugador1 = Map.ofEntries(Map.entry());
+}
