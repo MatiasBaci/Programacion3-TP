786e1ef87b7d226c5fd776452ff4ec9d7c148ed6
MatiasBaci <matibaciga@gmail.com>
2023-11-22 00:01:22 -0300

    Bugfix

--- TpJava/src/main/java/orgFiuba/tpjava/MainJavaFX.java
+++ TpJava/src/main/java/orgFiuba/tpjava/MainJavaFX.java
@@ -20,7 +20,6 @@ public class MainJavaFX extends Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainJavaFX.class.getResource("juego-view.fxml"));
         VBox root = fxmlLoader.load();
-        //JuegoController1 juegoController = fxmlLoader.getController();
         JuegoController juegoController = fxmlLoader.getController();
         juegoController.setJuego(juego);
 
@@ -29,18 +28,6 @@ public class MainJavaFX extends Application {
 
         stage.setScene(scene);
         stage.show();
-
-        //Image imagePikachu = new Image(getClass().getResource("/orgFiuba/tpjava/Imagenes/pokemonesEjemplo/Frente/pikachu.png").toString());
-        //ImageView imageView = new ImageView();
-        //imageView.setImage(imagePikachu);
-
-
-        /*PokemonImageFactory pokemonImageFactory = new PokemonImageFactory();
-        Pokemon pikachu = new Pokemon("Pikachu", 50, "Electrico", "Las bolsas de las mejillas están llenas de electricidad, que libera cuando se siente amenazado.",
-                110, 110, 70, 75);
-        pokemonImageFactory.createPokemonImageView(pikachu);
-        JuegoController1 juegoController = fxmlLoader.getController();
-        //juegoController.setPokemonImageFactory(pokemonImageFactory);*/
     }
 
 
